24b014f1f474030c1fc4d432ec53eb45459e15c5
Mike Noel <mike.e.noel3@gmail.com>
2020-04-10 20:17:14 -0500

    pushing up server changes

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 var express = require('express');
 var cors = require('cors');
+var uuidv4 = require('uuid/v4');
+var squareConnect = require('square-connect')
 var port = process.env.PORT || 8080;
 var app = express();
+app.use(express.json());
 app.use(cors())
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
-});
+const defaultClient = squareConnect.ApiClient.instance;
+defaultClient.basePath = 'https://connect.squareupsandbox.com'
+oauth2 = defaultClient.authentications['oauth2'];
+oauth2.accessToken = "EAAAEFUv7oUBP6S6603wYwXwKDZh23Dlx-FWcQvWYhutG35hy9B_DOq8HUUJuAEo"
+
+app.post('/payments', async (req, res) => {
+    const request_params = req.body;
+
+    const idempotency_key = uuidv4();
+  
+    // Charge the customer's card
+    const payments_api = new squareConnect.PaymentsApi();
+    const request_body = {
+      source_id: request_params.nonce,
+      amount_money: {
+        amount: 100, // $1.00 charge
+        currency: 'USD'
+      },
+      idempotency_key: idempotency_key
+    };
+  
+    try {
+      const respone = await payments_api.createPayment(request_body);
+      const json = JSON.stringify(respone);
+      res.send(json)
+    } catch (error) {
+      res.send(error)
+    } 
+})
+
 app.listen(port, function () {
- console.log('Example app listening on port !');
+ console.log('Example app listening on port ' + port);
 });
